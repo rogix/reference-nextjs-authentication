a5efddbdda74ef4b8caa0ae01575078e53da4d55
Rogix <12768403+rogix@users.noreply.github.com>
2022-03-09 09:49:46 -0300

    feat: permission hook

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,12 +1,17 @@
 import { Box, Flex, Heading } from '@chakra-ui/react'
 import { useContext, useEffect } from 'react'
 import { AuthContext } from '../contexts/AuthContext'
+import { useCan } from '../hooks/useCan'
 import { setupAPIClient } from '../services/api'
 import { authApi } from '../services/apiClient'
 import { withSSRAuth } from '../utils/withSSRAuth'
 
 export default function Dashboard() {
   const { user } = useContext(AuthContext)
+
+  const useCanSeeMetrics = useCan({
+    permissions: ['metrics.list'],
+  })
 
   useEffect(() => {
     authApi
@@ -25,6 +30,8 @@ export default function Dashboard() {
     <Box bg="gray.700" h="100vh" color="white">
       <Flex flexDirection="column" align="center" justify="center" h="100%">
         <Heading>Hello: {user?.email}</Heading>
+
+        {useCanSeeMetrics && <p>You can see metrics</p>}
       </Flex>
     </Box>
   )
